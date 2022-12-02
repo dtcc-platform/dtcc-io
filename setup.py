b7c53d8a47e69b56bf7b4981cf5eac9898ae20df
Dag Wästberg <dwastberg@gmail.com>
2022-12-02 13:34:59 +0100

    initial pybind in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages, find_namespace_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
 from pathlib import Path
 import os, pathlib, sys
 
@@ -15,9 +17,24 @@ install_requires = [
     "meshio>=5.3.0<6.0.0",
 ]
 
+
+pybind_lib_root = (Path(__file__).parent / "cpp").resolve()
+ext_modules = [
+    Pybind11Extension(
+        "create_pb_pointcloud",
+        [str(pybind_lib_root / "src" / "create_pb_pointcloud.cpp")],
+        include_dirs=[
+            str(pybind_lib_root / "src"),
+            "/usr/local/include"
+        ],
+        extra_compile_args=["-std=c++1y"],
+    ),
+]
+package_data = {"": ["*.so", "*.dll", "*.dylib"]}
+
 setup(
     name="dtcc-io",
-    version="0.1.4",
+    version="0.1.5",
     description="IO for DTCC",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
@@ -25,7 +42,11 @@ setup(
     author="Dag Wästerberg",
     author_email="dwastberg@gmail.com",
     license="MIT",
-    packages=find_namespace_packages(include=['dtcc.*'])
+    packages=find_namespace_packages(include=['dtcc.*']),
+    package_data=package_data,
+    install_requires=  install_requires,
+    python_requires = ">=3.8,<4.0",
+    #ext_modules= ext_modules,
 )
 
 
