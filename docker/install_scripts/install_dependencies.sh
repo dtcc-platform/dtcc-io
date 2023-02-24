930506db21ef77199305cb1b356ad2bd9b59583e
Dag Wästberg <dwastberg@gmail.com>
2023-02-24 14:44:40 +0100

    fix dependencies

--- docker/install_scripts/install_dependencies.sh
+++ docker/install_scripts/install_dependencies.sh
@@ -12,7 +12,7 @@ apt-get update && apt-get install -y locales \
     libassimp-dev \
     protobuf-compiler
 
-./install_py_libs.sh
+
 #./install_assimp.sh
 
 #these seem necessary for the python bindings to work
@@ -22,3 +22,5 @@ fi
 if [-f /usr/lib/aarch64-linux-gnu/libassimp.so.5]; then
     ln -s /usr/lib/aarch64-linux-gnu/libassimp.so.5 /usr/lib/
 fi
+
+./install_py_libs.sh
