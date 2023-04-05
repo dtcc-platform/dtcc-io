745766564ae50f698f2516b449cc7612afeb11ad
Anders Logg <logg@chalmers.se>
2023-04-05 15:05:00 +0200

    Extend __all__

--- src/dtcc_io/__init__.py
+++ src/dtcc_io/__init__.py
@@ -5,22 +5,27 @@ import dtcc_io.mesh.mesh as mesh
 import dtcc_io.pointcloud as pointcloud
 import dtcc_io.view as view
 
+# FIXME: Why do we have read/write? Shouldn't we just have load/save?
+
 read_surface3d = mesh.load_surface3d
 write_surface3d = mesh.save_surface3d
 load_surface3d = mesh.load_surface3d
 save_surface3d = mesh.save_surface3d
 load_mesh = mesh.load_surface3d
 save_mesh = mesh.save_surface3d
-
 load_mesh3d = mesh.load_mesh3d
 save_mesh3d = mesh.save_mesh3d
-
 save_mesh2d = mesh.save_mesh2d
 
 read_pointcloud = pointcloud.load
 write_pointcloud = pointcloud.save
 load_pointcloud = pointcloud.load
 save_pointcloud = pointcloud.save
+
+read_elevationmodel = elevationmodel.load
+write_elevationmodel = elevationmodel.save
+load_elevationmodel = elevationmodel.load
+save_elevationmodel = elevationmodel.save
 
 read_citymodel = citymodel.load
 write_citymodel = citymodel.save
@@ -29,10 +34,13 @@ save_citymodel = citymodel.save
 load_footprints = citymodel.load
 save_footprints = citymodel.save
 
-read_elevationmodel = elevationmodel.load
-write_elevationmodel = elevationmodel.save
-load_elevationmodel = elevationmodel.load
-save_elevationmodel = elevationmodel.save
-
 view_citymodel = view.view_citymodel
-__all__ = ['bounds', 'citymodel', 'elevationmodel', 'mesh', 'pointcloud']
+
+# __all__ = ['bounds', 'citymodel', 'elevationmodel', 'mesh', 'pointcloud']
+
+__all__ = ['load_mesh', 'save_mesh',
+           'load_pointcloud', 'save_pointcloud',
+           'load_elevationmodel', 'save_elevationmodel',
+           'load_citymodel', 'save_citymodel',
+           'load_footprints', 'save_footprints',
+           'view_citymodel']
