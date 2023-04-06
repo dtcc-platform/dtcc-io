cfebe871028db14b875c4be2c29ee79232b2de46
Dag Wästberg <dwastberg@gmail.com>
2023-04-06 11:58:31 +0200

    fix search path order

--- cmake/FindDTCCModel.cmake
+++ cmake/FindDTCCModel.cmake
@@ -4,21 +4,21 @@ message(STATUS "Looking for DTCCModel")
 
 find_path(DTCCMODEL_INCLUDE_DIRS
     NAMES dtcc.pb.h
-    PATHS /usr/local/include/dtcc_model
+    PATHS ${CMAKE_PREFIX_PATH}/include/dtcc_model
+    ${CMAKE_PREFIX_PATH}/include
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${CMAKE_INSTALL_PREFIX}/include/dtcc_model
+    /usr/local/include/dtcc_model
     /usr/local/include
     /usr/include/dtcc_model
-    /usr/include
-    ${CMAKE_INSTALL_PREFIX}/include
-    ${CMAKE_PREFIX_PATH}/include/dtcc_model
-    ${CMAKE_PREFIX_PATH}/include
-    ${CMAKE_PREFIX_PATH}/include/dtcc_model)
+    /usr/include)
 
 find_library(DTCCMODEL_LIBRARIES
     NAMES dtccproto
-    PATHS /usr/local/lib
-    /usr/lib
-    ${CMAKE_PREFIX_PATH}/lib
+    PATHS ${CMAKE_PREFIX_PATH}/lib
     ${CMAKE_INSTALL_PREFIX}/lib
+    /usr/local/lib
+    /usr/lib
 )
 
 find_package_handle_standard_args(DTCCModel
