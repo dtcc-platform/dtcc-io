8f0a5b019baeeac7faa301d6d42b4d764f897941
Dag Wastberg <cit-wastberg@develop.dtcc>
2023-04-11 15:06:47 +0200

    search .local for dtccmodel

--- cmake/FindDTCCModel.cmake
+++ cmake/FindDTCCModel.cmake
@@ -3,20 +3,19 @@ include(FindPackageHandleStandardArgs)
 message(STATUS "Looking for DTCCModel")
 
 find_path(DTCCMODEL_INCLUDE_DIRS
-    NAMES dtcc.pb.h
+    NAMES dtcc_model/dtcc.pb.h
     PATHS ${CMAKE_PREFIX_PATH}/include/dtcc_model
     ${CMAKE_PREFIX_PATH}/include
     ${CMAKE_INSTALL_PREFIX}/include
-    ${CMAKE_INSTALL_PREFIX}/include/dtcc_model
-    /usr/local/include/dtcc_model
+    $ENV{HOME}/.local/include
     /usr/local/include
-    /usr/include/dtcc_model
     /usr/include)
 
 find_library(DTCCMODEL_LIBRARIES
     NAMES dtccproto
     PATHS ${CMAKE_PREFIX_PATH}/lib
     ${CMAKE_INSTALL_PREFIX}/lib
+    $ENV{HOME}/.local/lib
     /usr/local/lib
     /usr/lib
 )
