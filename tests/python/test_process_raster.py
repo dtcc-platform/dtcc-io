96546aa1647f55d9503797b2009c7fb472be38bc
Dag Wästberg <dwastberg@gmail.com>
2023-05-05 16:19:38 +0200

    raster resampling

--- tests/python/test_process_raster.py
+++ tests/python/test_process_raster.py
@@ -19,3 +19,29 @@ class TestFillHoles(unittest.TestCase):
         self.assertEqual(dem.data[5, 5], -9999)
         dem = dem.fill_holes()
         self.assertGreater(dem.data[5, 5], 0)
+
+
+class TestResample(unittest.TestCase):
+    def test_upscale(self):
+        dem = io.load_raster("data/test_dem.tif")
+        orig_height = dem.height
+        orig_width = dem.width
+        dem = dem.resample(scale=2)
+        self.assertEqual(dem.height, orig_height * 2)
+        self.assertEqual(dem.width, orig_width * 2)
+
+    def test_downscale(self):
+        dem = io.load_raster("data/test_dem.tif")
+        orig_height = dem.height
+        orig_width = dem.width
+        dem = dem.resample(scale=0.5)
+        self.assertEqual(dem.height, orig_height // 2)
+        self.assertEqual(dem.width, orig_width // 2)
+
+    def test_resample_nearest(self):
+        dem = io.load_raster("data/test_dem.tif")
+        dem = dem.resample(scale=4, method="nearest")
+        self.assertEqual(dem.data[0, 0], dem.data[0, 1])
+        self.assertEqual(dem.data[0, 0], dem.data[1, 0])
+        self.assertEqual(dem.data[0, 0], dem.data[1, 1])
+        self.assertEqual(dem.data[0, 0], dem.data[2, 2])
