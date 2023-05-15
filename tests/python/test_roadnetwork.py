6176ce7c02f59d79740749b20a343aa56a92a5dd
Dag Wästberg <dwastberg@gmail.com>
2023-05-15 16:24:43 +0200

    add test

--- tests/python/test_roadnetwork.py
+++ tests/python/test_roadnetwork.py
@@ -25,3 +25,25 @@ class TestRoadnetworkIO(unittest.TestCase):
         with open(outfile.name) as f:
             data = json.load(f)
         self.assertEqual(len(data["features"]), 244)
+
+    def test_vertices(self):
+        rn = io.load_roadnetwork(self.roadnetwork_shp_file, type_field="KATEGORI")
+        r0 = rn.roads[0]
+        self.assertEqual(len(r0.road_vertices), len(r0.road_geometry.coords))
+        r123 = rn.roads[123]
+        self.assertEqual(len(r123.road_vertices), len(r123.road_geometry.coords))
+
+        verts = rn.vertices[r0.road_vertices]
+        self.assertAlmostEqual(
+            verts[0][0], list(r0.road_geometry.coords)[0][0], places=3
+        )
+        self.assertAlmostEqual(
+            verts[0][1], list(r0.road_geometry.coords)[0][1], places=3
+        )
+
+        self.assertAlmostEqual(
+            verts[12][0], list(r0.road_geometry.coords)[12][0], places=3
+        )
+        self.assertAlmostEqual(
+            verts[12][1], list(r0.road_geometry.coords)[12][1], places=3
+        )
