d862eeee74b957e05b5999ed8dbdaa1d2111798e
Dag Wästberg <dwastberg@gmail.com>
2024-01-26 17:04:52 +0100

    load cityjson

--- src/dtcc_io/cityjson/cityjson.py
+++ src/dtcc_io/cityjson/cityjson.py
@@ -1,150 +1,44 @@
-import copy
+from dtcc_io.cityjson.utils import get_terrain_mesh, get_buildings
+from dtcc_model.object.city import NewCity as City
+from dtcc_model.geometry import Bounds
+import numpy as np
+from pathlib import Path
 import json
 
-import dtcc_model as model
-from dataclasses import dataclass, field
-import numpy as np
-
-
-# intermediate stucts used while parsing
-@dataclass
-class CityBuilding:
-    uuid: str
-    root: dict
-    children: list[dict] = field(default_factory=list)
-
-
-@dataclass
-class CityJSONParts:
-    verts: np.ndarray = field(default_factory=lambda: np.empty((0, 3)))
-    root_buildings: dict = field(default_factory=dict)
-    root_objects: dict = field(default_factory=dict)
-    city_objects: dict = field(default_factory=dict)
-    bounds: model.geometry.Bounds = field(default_factory=model.geometry.Bounds)
-
-
-def load_cityjson(path: str) -> model.city:
-    """Load a CityJSON file into a CityModel."""
-    with open(path, "r") as f:
-        cj = json.load(f)
-    if "type" not in cj or cj["type"] != "CityJSON":
-        raise ValueError("Not a CityJSON file")
-    return parse_cityjson(cj)
-
-
-def parse_cityjson(cj: dict) -> CityJSONParts:
-    """Parse a CityJSON file into a CityJSONParts object."""
-    cj_obj = CityJSONParts()
-    if "transform" in cj:
-        scale = np.array(cj["transform"]["scale"])
-        translate = np.array(cj["transform"]["translate"])
+def setup_city(cj_obj: dict):
+    city = City()
+    if "transform" in cj_obj:
+        scale = np.array(cj_obj["transform"]["scale"])
+        translate = np.array(cj_obj["transform"]["translate"])
     else:
         scale = np.array([1, 1, 1])
         translate = np.array([0, 0, 0])
-    if "metadata" in cj:
-        if "geographicalExtent" in cj["metadata"]:
-            extent = cj["metadata"]["geographicalExtent"]
-            cj_obj.bounds = model.geometry.Bounds(
+    if "metadata" in cj_obj:
+        if "geographicalExtent" in cj_obj["metadata"]:
+            extent = cj_obj["metadata"]["geographicalExtent"]
+            city.geometry['bounds'] = Bounds(
                 extent[0], extent[1], extent[3], extent[4]
             )
-    cj_obj.verts = np.array(cj["vertices"]) * scale + translate
-    cj_obj.city_objects.update(cj["CityObjects"])
+    verts = np.array(cj_obj["vertices"]) * scale + translate
 
-    for k, v in cj["CityObjects"].items():
-        if "parents" not in v:
-            if v["type"] == "Building":
-                cj_obj.root_buildings[k] = v
-            else:
-                cj_obj.root_objects[k] = v
-
-    return cj_obj
+    return city, verts
 
 
-def parse_buildings(cj_obj):
-    buildings = []
-    for k, v in cj_obj.root_buildings.items():
-        cb = CityBuilding(k, v)
-        # print(v)
-        for child in v.get("children", []):
-            # print(children)
-            cb.children.append(cj_obj.city_objects[child])
-        buildings.append(cb)
-    return buildings
+def load(cityjson_path: str) -> City:
+    """Load a CityJSON file into a City object."""
+    cityjson_path = Path(cityjson_path)
+    if not cityjson_path.exists():
+        raise FileNotFoundError(f"File {cityjson_path} not found")
+    with open(cityjson_path, "r") as f:
+        cj = json.load(f)
+    if "type" not in cj or cj["type"] != "CityJSON":
+        raise ValueError("Not a CityJSON file")
+    city, verts = setup_city(cj)
+    cj_obj = cj["CityObjects"]
+    tin = get_terrain_mesh(cj_obj, verts)
+    if len(tin.vertices) > 0:
+        city.geometry['terrain_mesh'] = tin
+    get_buildings(cj_obj, verts, city)
+    pass
+    return city
 
-def get_geom_to_use(geom_list, lod=2, prefer_surface=True) -> dict:
-    """
-Get the geometry we want to use from a list of possible geometries
-    Parameters
-    ----------
-    geom_list: list of possible geometries
-    lod: int, prefered level of detail we want, prefer lower to higher if we cannot find exact lod
-    prefer_surface: bool, if True, prefer surface to solid
-
-    Returns
-    -------
-    geom: dict, the geometry we want to use
-
-    """
-    if len(geom_list) == 0:
-        return {}
-    if len(geom_list) == 1:
-        return geom_list[0]
-
-    candidates = []
-    while len(candidates) == 0:
-        for g in geom_list:
-            if g["lod"] == lod:
-                candidates.append(g)
-        lod -= 1
-        if lod < 0:
-            candidates = geom_list
-    if len(candidates) == 1:
-        return candidates[0]
-    for geom in candidates:
-        if "Surface" in geom["type"] and prefer_surface:
-            return geom
-        elif "Solid" in geom["type"] and not prefer_surface:
-            return geom
-    return candidates[0]
-
-
-
-
-
-def get_building_geometry(cj_obj, buildings, lod=2):
-    buildings_geom = []
-    for b in buildings:
-        building_ms = []
-        if 'geometry' in b.root:
-            building_geometry = b.root['geometry']
-            geom = get_geom_to_use(building_geometry, lod=lod)
-            ms = build_multisurface(cj_obj, geom)
-            ms.properties['semantics'] = geom.get("semantics", {})
-            building_ms.append(ms)
-        for c in b.root.get("children", []):
-            child = cj_obj.city_objects[c]
-            geom = child["geometry"]
-            geom = get_geom_to_use(geom, lod=lod)
-            ms = build_multisurface(cj_obj, geom)
-            ms.properties['semantics'] = geom.get("semantics", {})
-            building_ms.append(ms)
-        buildings_geom.append(building_ms)
-    return buildings_geom
-
-
-def build_multisurface(cj_obj, geom):
-    """Build a MultiSurface from a CityJSON geometry."""
-    ms = model.geometry.MultiSurface()
-    if geom["type"] == "MultiSurface" or geom["type"] == "CompositeSurface":
-        boundaries = geom["boundaries"]
-    elif geom["type"] == "Solid":
-        boundaries = geom["boundaries"][0]
-    else:
-        raise ValueError(f"Unhandled geometry type {geom['type']}")
-    for surface in boundaries:
-        s = model.geometry.Surface()
-        outer = surface[0]
-        inner = surface[1:]
-        s.vertices = cj_obj.verts[outer]
-        ms.surfaces.append(s)
-    return ms
---
+++ src/dtcc_io/cityjson/utils.py
@@ -0,0 +1,148 @@
+import numpy as np
+
+from dtcc_model import Mesh, MultiSurface, Surface, NewBuilding as Building, NewCity as City
+from dtcc_model.object.building import BuildingPart
+
+
+def tin_geom_to_mesh(tin_geom, verts) -> Mesh:
+    mesh = Mesh()
+    faces = []
+    for geom in tin_geom:
+        vert_dict = {}
+        faces += [face[0] for face in geom["boundaries"]]
+        for face in faces:
+            vert_dict[face[0]] = verts[face[0]]
+            vert_dict[face[1]] = verts[face[1]]
+            vert_dict[face[2]] = verts[face[2]]
+
+    fv_map = [(f, v) for f, v in vert_dict.items()]
+    fv_map.sort(key=lambda x: x[0])
+    vert_map = {f: i for i, f in enumerate([f for f, v in fv_map])}
+    mesh.vertices = np.array([v for f, v in fv_map])
+    mesh.faces = np.array([[vert_map[f] for f in face] for face in faces])
+    return mesh
+
+
+def get_terrain_mesh(cj_obj: dict, verts: np.ndarray) -> Mesh:
+    tin_geom = []
+    for geom in cj_obj.values():
+        if geom['type'] == 'TINRelief':
+            tin_geom.append(tin_geom_to_mesh(geom, verts))
+    if len(tin_geom) == 0:
+        return Mesh()
+    if len(tin_geom) == 1:
+        return tin_geom[0]
+    else:
+        #TODO: implement mesh merging i wrangler
+        # return tin_geom[0].merge(tin_geom[1:])
+        return tin_geom[0]
+
+
+def build_multisurface(geom, verts):
+    """Build a MultiSurface from a CityJSON geometry."""
+    ms = MultiSurface()
+    if not isinstance(geom, dict):
+        pass
+    if geom["type"] == "MultiSurface" or geom["type"] == "CompositeSurface":
+        boundaries = geom["boundaries"]
+    elif geom["type"] == "Solid":
+        boundaries = geom["boundaries"][0]
+    else:
+        raise ValueError(f"Unhandled geometry type {geom['type']}")
+    for surface in boundaries:
+        s = Surface()
+        outer = surface[0]
+        inner = surface[1:]
+        s.vertices = verts[outer]
+        ms.surfaces.append(s)
+    return ms
+
+
+def get_geom_to_use(geom_list, lod=2, prefer_surface=True) -> dict:
+    """
+Get the geometry we want to use from a list of possible geometries
+    Parameters
+    ----------
+    geom_list: list of possible geometries
+    lod: int, prefered level of detail we want, prefer lower to higher if we cannot find exact lod
+    prefer_surface: bool, if True, prefer surface to solid
+
+    Returns
+    -------
+    geom: dict, the geometry we want to use
+
+    """
+    if len(geom_list) == 0:
+        return {}
+    if len(geom_list) == 1:
+        return geom_list[0]
+
+    candidates = []
+    while len(candidates) == 0:
+        for g in geom_list:
+            if g["lod"] == lod:
+                candidates.append(g)
+        lod -= 1
+        if lod < 0:
+            candidates = geom_list
+    if len(candidates) == 1:
+        return candidates[0]
+    for geom in candidates:
+        if "Surface" in geom["type"] and prefer_surface:
+            return geom
+        elif "Solid" in geom["type"] and not prefer_surface:
+            return geom
+    return candidates[0]
+
+
+def get_building_geometry(cj_obj,building, verts, lod=2):
+    building_root_geom = None
+    building_children = []
+    if 'geometry' in building:
+        building_geometry = building['geometry']
+        geom = get_geom_to_use(building_geometry, lod=lod)
+        ms = build_multisurface(geom, verts)
+        #ms.properties['semantics'] = geom.get("semantics", {})
+        building_root_geom = (geom,ms)
+    for c in building.get("children", []):
+        child = cj_obj[c]
+        geom = child["geometry"]
+        geom = get_geom_to_use(geom, lod=lod)
+        ms = build_multisurface(geom,verts)
+        building_children.append((geom,ms))
+    return building_root_geom, building_children
+
+
+def build_dtcc_building(cj_obj,uuid, cj_building, verts, parent_city, lod=2):
+    building = Building()
+    building.parents[City] = [parent_city]
+    parent_city.children[Building].append(building)
+    building.id = uuid
+    building.attributes = cj_building.get('attributes',{})
+    building_root_geom, building_children = get_building_geometry(cj_obj, cj_building, verts, lod=lod)
+    if building_root_geom is not None:
+        geom, ms = building_root_geom
+        lod = geom.get('lod', 1)
+        building.geometry[f'lod{lod}'] = ms
+    for geom, ms in building_children:
+        lod = geom.get('lod', 1)
+        building_part = BuildingPart()
+        building_part.parents[Building] = [building]
+        building.children[BuildingPart].append(building_part)
+        building_part.geometry[f'lod{lod}'] = ms
+
+
+def get_root_buildings(cj_obj: dict):
+    cj_root_buildings = []
+    for k,v in cj_obj.items():
+        if "parents" not in v:
+            if v["type"] == "Building":
+                cj_root_buildings.append((k,v))
+    return cj_root_buildings
+
+
+def get_buildings(cj_obj: dict, verts: np.ndarray, parent_city: City, lod=2) -> [Building]:
+    root_buildings = get_root_buildings(cj_obj)
+    buildings = []
+    for uuid,v in root_buildings:
+        buildings.append(build_dtcc_building(cj_obj,uuid,v, verts, parent_city, lod=lod))
